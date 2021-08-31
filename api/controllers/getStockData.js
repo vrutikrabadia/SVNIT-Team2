bb206d4ce34d89e00c8044d962e869dff776761a
AvanishCodes <avanishcodes@gmail.com>
2021-08-31 20:41:56 +0530

    Add code for adding a particular stock data to test scalability

--- api/controllers/getStockData.js
+++ api/controllers/getStockData.js
@@ -4,6 +4,11 @@
 const mongoose = require("mongoose");
 const Stock = require("../models/stock.model");
 
+/**
+ * @api {get} /stock/:symbol Get stock data
+ * @param {req} req - request object containing stock symbol in path
+ * @param {res} res - response object containing stock data
+ */
 exports.getStockData = async function (req, res) {
   console.log("getStockData");
   console.log(req.params.stock);
@@ -11,12 +16,25 @@ exports.getStockData = async function (req, res) {
   Stock.find({}, (err, stocks) => {
     if (err) {
       console.log(err);
+      return res.status(404).json({
+        status: "error",
+        message: "No stock was found with this company code!",
+      });
     } else {
-      return res.json(stocks);
+      return res.status(200).json({ status: "success", stocks: stocks });
     }
   });
   // console.log(stocks);
   // return res.status(200).json({
   //   status: "success",
   // });
+};
+
+exports.getSimilarStockNames = async function (req, res) {
+  let stocks = await Stock.find({
+    symbol: { $regex: req.params.stock_pattern },
+  }).select("symbol name");
+  if (stocks.length > 0)
+    res.status(200).json({ status: "success", message: stocks });
+  else res.status(404).json({ status: "error", message: "No stocks found" });
 };
